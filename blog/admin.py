2625ecb3cb2978533355ce502e75a3dfda75f908
Roibéard R Coughlan <roibeard.c@outlook.com>
2022-05-25 16:19:55 +0000

    Add new template add_blog with Template content, urls & views

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,28 @@
+from django import forms
 from .models import Blog
 
 
-class BlogAdmin(admin.ModelAdmin):
-    list_display = (
-        'blog_title',
-        'blog_content',
-        'author',
-        'image',
-        'date_created',
-    )
-    ordering = ['-date_created']
+class BlogForm(forms.ModelForm):
+    """ Create BlogForm class for Admin """
 
+    class Meta:
+        """ Update Class Meta Data """
+        model = Blog
+        fields = '__all__'
 
-admin.site.register(Blog, BlogAdmin)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'blog_title': 'e.g Now that is a Mojito',
+            'blog_content': 'e.g Fresh mint leaves with crushed ice',
+            'author': 'Joe Bloggs',
+            'image': 'Image Upload',
+            'date_created': '25th of September 2022'
+        }
+        self.fields['blog_title'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Blog
+from .forms import BlogForm
+
 
 def blog(request):
     """ A view to render the blogs page """
@@ -9,6 +13,46 @@ def blog(request):
     template = 'blog/blogs.html'
     context = {
         'blog': blog,
+    }
+
+
+    return render(request, template, context)
+
+
+def blog_detail(request, blog_id):
+    """ A view to display individual blog posts """
+
+    blog = get_object_or_404(Blog, pk=blog_id)
+
+    template = 'blog/blog_detail.html'
+    context = {
+        'blog': blog,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def add_blog(request):
+    """ Admin users can add a blog to the site """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, you need to be logged in to do this')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES)
+        if form.is_valid():
+            blog = form.save()
+            messages.success(request, 'Successfully added blog!')
+            return redirect(reverse('blog_detail', args=[blog.id]))
+        else:
+            messages.error(request, 'Failed to add blog. Please ensure the form is valid')
+    else:
+        form = BlogForm()
+
+    template = 'blog/add_blog.html'
+    context = {
+        'form': form,
     }
 
     return render(request, template, context)
