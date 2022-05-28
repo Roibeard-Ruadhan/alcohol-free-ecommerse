95320fea23a0c9b20a7c80c82ca5776e9b24584f
Roibéard R Coughlan <roibeard.c@outlook.com>
2022-05-28 20:00:02 +0000

    Round edges of all borders

--- profiles/forms.py
+++ profiles/forms.py
@@ -31,6 +31,5 @@ class UserProfileForm(forms.ModelForm):
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = ('border-black '
-                                                        'rounded-0 '
                                                         'profile-form-input')
             self.fields[field].label = False
