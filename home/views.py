e6ef457bf035a6802a23fd93b3cc120c300eb740
Roibéard R Coughlan <roibeard.c@outlook.com>
2022-06-02 08:13:27 +0000

    Update error 500 handler

--- home/views.py
+++ home/views.py
@@ -9,7 +9,10 @@ def index(request):
 
 
 def error_404(request, exception):
-    return render(request, '404.html')
+    return render(request, '404.html', status=404)
+
+def error_500(request, *args, **argv):
+    return render(request, '500.html', status=500)
 
 
 
