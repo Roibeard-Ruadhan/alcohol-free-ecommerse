c2a5c5ec514deb191eefe67d15e954d53ddcfd58
Roibéard R Coughlan <roibeard.c@outlook.com>
2023-01-19 17:52:53 +0000

    Remove unused import from urls.py

--- alcohol_free/urls.py
+++ alcohol_free/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import handler403, handler404, handler500
 from home.views import handler_403, handler_404, handler_500
 
 urlpatterns = [
