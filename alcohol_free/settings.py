27b54821ad275dd149e7d414e8455b278f2369ca
Roibéard R Coughlan <roibeard.c@outlook.com>
2023-01-25 18:13:07 +0000

    Add migrations for checkout & profiles

--- alcohol_free/settings.py
+++ alcohol_free/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'django_countries',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
